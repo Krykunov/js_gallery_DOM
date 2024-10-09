1bb3f004c431aeab0cce8b67859daae0ba61abf3
Igor <4430704@gmail.com>
2024-10-09 18:12:47 +0300

    make build

---
+++ index.8f018ba1.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__large-img");document.querySelectorAll(".list-item").forEach(function(t){t.addEventListener("click",function(t){t.preventDefault();var r=t.target.closest("a").href;e.src=r})});
+//# sourceMappingURL=index.8f018ba1.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const largeImg = document.querySelector(".gallery__large-img");
+const galleryItems = document.querySelectorAll(".list-item");
+galleryItems.forEach((item)=>{
+    item.addEventListener("click", (e)=>{
+        e.preventDefault();
+        const imgSrc = e.target.closest("a").href;
+        largeImg.src = imgSrc;
+    });
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
